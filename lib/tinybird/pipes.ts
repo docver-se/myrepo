183b8eca59f0fcbb6aa0f4206908212cbaf65bb4
Giovani de SOUZA <desouzagiovani9@gmail.com>
2025-07-05 02:02:42 +0100

    feat: Setup tinybird

--- lib/tinybird/pipes.ts
+++ lib/tinybird/pipes.ts
@@ -7,7 +7,7 @@ import { WEBHOOK_TRIGGERS } from "../webhook/constants";
 const tb = new Tinybird({ token: process.env.TINYBIRD_TOKEN! });
 
 export const getTotalAvgPageDuration = tb.buildPipe({
-  pipe: "get_total_average_page_duration__v5",
+  pipe: "get_total_average_page_duration",
   parameters: z.object({
     documentId: z.string(),
     excludedLinkIds: z.string().describe("Comma separated linkIds"),
@@ -15,14 +15,12 @@ export const getTotalAvgPageDuration = tb.buildPipe({
     since: z.number(),
   }),
   data: z.object({
-    versionNumber: z.number().int(),
-    pageNumber: z.string(),
     avg_duration: z.number(),
   }),
 });
 
 export const getViewPageDuration = tb.buildPipe({
-  pipe: "get_page_duration_per_view__v5",
+  pipe: "get_page_duration_per_view",
   parameters: z.object({
     documentId: z.string(),
     viewId: z.string(),
@@ -30,13 +28,13 @@ export const getViewPageDuration = tb.buildPipe({
     until: z.number().optional(),
   }),
   data: z.object({
-    pageNumber: z.string(),
-    sum_duration: z.number(),
+    page: z.string(),
+    duration: z.number(),
   }),
 });
 
 export const getTotalDocumentDuration = tb.buildPipe({
-  pipe: "get_total_document_duration__v1",
+  pipe: "get_total_document_duration",
   parameters: z.object({
     documentId: z.string(),
     excludedLinkIds: z.string().describe("Comma separated linkIds"),
@@ -50,7 +48,7 @@ export const getTotalDocumentDuration = tb.buildPipe({
 });
 
 export const getTotalLinkDuration = tb.buildPipe({
-  pipe: "get_total_link_duration__v1",
+  pipe: "get_total_link_duration",
   parameters: z.object({
     linkId: z.string(),
     documentId: z.string(),
@@ -60,12 +58,11 @@ export const getTotalLinkDuration = tb.buildPipe({
   }),
   data: z.object({
     sum_duration: z.number(),
-    view_count: z.number(),
   }),
 });
 
 export const getTotalViewerDuration = tb.buildPipe({
-  pipe: "get_total_viewer_duration__v1",
+  pipe: "get_total_viewer_duration",
   parameters: z.object({
     viewIds: z.string().describe("Comma separated viewIds"),
     since: z.number(),
@@ -76,24 +73,8 @@ export const getTotalViewerDuration = tb.buildPipe({
   }),
 });
 
-export const getViewUserAgent_v2 = tb.buildPipe({
-  pipe: "get_useragent_per_view__v2",
-  parameters: z.object({
-    documentId: z.string(),
-    viewId: z.string(),
-    since: z.number(),
-  }),
-  data: z.object({
-    country: z.string(),
-    city: z.string(),
-    browser: z.string(),
-    os: z.string(),
-    device: z.string(),
-  }),
-});
-
 export const getViewUserAgent = tb.buildPipe({
-  pipe: "get_useragent_per_view__v3",
+  pipe: "get_useragent_per_view",
   parameters: z.object({
     viewId: z.string(),
   }),
@@ -107,7 +88,7 @@ export const getViewUserAgent = tb.buildPipe({
 });
 
 export const getTotalDataroomDuration = tb.buildPipe({
-  pipe: "get_total_dataroom_duration__v1",
+  pipe: "get_total_dataroom_duration",
   parameters: z.object({
     dataroomId: z.string(),
     excludedLinkIds: z.array(z.string()),
@@ -115,26 +96,26 @@ export const getTotalDataroomDuration = tb.buildPipe({
     since: z.number(),
   }),
   data: z.object({
-    viewId: z.string(),
-    sum_duration: z.number(),
+    total_duration: z.number(),
   }),
 });
 
 export const getDocumentDurationPerViewer = tb.buildPipe({
-  pipe: "get_document_duration_per_viewer__v1",
+  pipe: "get_document_duration_per_viewer",
   parameters: z.object({
-    documentId: z.string(),
+    doc_id: z.string(),
     viewIds: z.string().describe("Comma separated viewIds"),
   }),
   data: z.object({
-    sum_duration: z.number(),
+    link_id: z.string(),
+    total_duration: z.number(),
   }),
 });
 
 export const getWebhookEvents = tb.buildPipe({
-  pipe: "get_webhook_events__v1",
+  pipe: "get_webhook_events",
   parameters: z.object({
-    webhookId: z.string(),
+    webhook_id: z.string(),
   }),
   data: z.object({
     event_id: z.string(),
@@ -150,9 +131,9 @@ export const getWebhookEvents = tb.buildPipe({
 });
 
 export const getVideoEventsByDocument = tb.buildPipe({
-  pipe: "get_video_events_by_document__v1",
+  pipe: "get_video_events_by_document",
   parameters: z.object({
-    document_id: z.string(),
+    documentId: z.string(),
   }),
   data: z.object({
     timestamp: z.string(),
@@ -169,10 +150,9 @@ export const getVideoEventsByDocument = tb.buildPipe({
 });
 
 export const getVideoEventsByView = tb.buildPipe({
-  pipe: "get_video_events_by_view__v1",
+  pipe: "get_video_events_by_view",
   parameters: z.object({
-    document_id: z.string(),
-    view_id: z.string(),
+    viewId: z.string(),
   }),
   data: z.object({
     timestamp: z.string(),
@@ -183,10 +163,9 @@ export const getVideoEventsByView = tb.buildPipe({
 });
 
 export const getClickEventsByView = tb.buildPipe({
-  pipe: "get_click_events_by_view__v1",
+  pipe: "get_click_events_by_view",
   parameters: z.object({
-    document_id: z.string(),
-    view_id: z.string(),
+    viewId: z.string(),
   }),
   data: z.object({
     timestamp: z.string(),
